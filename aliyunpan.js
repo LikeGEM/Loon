af28131112d467b301a62c63867d5a2b5706d109
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-01 22:47:02 +0800

    Create aliyunpan.js

---
+++ aliyunpan.js
@@ -0,0 +1,10 @@
+#!name=阿里云盘
+#!desc=阿里云盘首页优化
+#!homepage=https://gitlab.com/ddgksf2013
+#!author= ddgksf2013
+#!icon=
+[MITM]
+hostname= api.aliyundrive.com
+
+[Script]
+http-response ^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/(users | apps\/widgets$) requires-body=1,script-path=https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Script/alidrive.js
