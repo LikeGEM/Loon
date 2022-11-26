4e52ea7f587ed2a86b5a6304d71b544c22611648
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-26 10:31:45 +0800

    Update aliyunpan.js

--- aliyunpan.js
+++ aliyunpan.js
@@ -1,10 +1,10 @@
 #!name=阿里云盘
 #!desc=阿里云盘首页优化
-#!homepage= https://gitlab.com/ddgksf2013
+#!homepage= https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Rewrite/AdBlock/StartUp.conf
 #!author= ddgksf2013
 #!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/ailiyunpan.png
 [MITM]
 hostname= api.aliyundrive.com
 
 [Script]
-http-response ^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/(users|apps\/widgets) requires-body=1,script-path=https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Script/alidrive.js
+http-response ^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/(users|apps\/widgets) requires-body=1,script-path= https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Script/alidrive.js
