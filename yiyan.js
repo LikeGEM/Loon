9861425104ee031ff7386ae1031976673dd0cb8b
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-29 00:43:04 +0800

    Create yiyan.js

---
+++ yiyan.js
@@ -0,0 +1,14 @@
+#!name=
+#!desc=
+#!homepage=
+#!author=
+#!icon=
+
+[Script]
+
+# ～ 一言解锁会员权限（2022-07-21）@ddgksf2013
+http-response ^https:\/\/app\.yiyan\.art\/yiyan\/ script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/yiyan.js, requires-body=true, timeout=10, tag=yiyan
+
+[MITM]
+
+hostname = app.yiyan.art
