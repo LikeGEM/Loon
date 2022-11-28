7ea3b4ed106d55e9c81fad484aa7b68f64c1ddcf
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-28 23:45:59 +0800

    Create duitang.js

---
+++ duitang.js
@@ -0,0 +1,14 @@
+#!name= 堆糖
+#!desc= 堆糖解锁订阅
+#!homepage= https://ocd0522.tk/ddgksf2013
+#!author= ddgksf2013
+#!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/duitang.png
+
+[Script]
+
+# ～ 堆糖解鎖會員權限（2022-05-05）@ddgksf2013
+http-response ^https?:\/\/api\.duitang\.com\/napi\/vienna\/graphic\/template script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/duitangpro.js, requires-body=true, timeout=10, tag=duitangpro
+
+[MITM]
+
+hostname = api.duitang.com
