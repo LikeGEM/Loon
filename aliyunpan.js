c98dad950b8e88c570f62f35803c59e666ee297c
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-28 14:02:25 +0800

    Update aliyunpan.js

--- aliyunpan.js
+++ aliyunpan.js
@@ -3,8 +3,11 @@
 #!homepage= https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Rewrite/AdBlock/StartUp.conf
 #!author= ddgksf2013
 #!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/ailiyunpan.png
-[MITM]
-hostname= api.aliyundrive.com
 
 [Script]
-http-response ^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/(users|apps\/widgets) requires-body=1,script-path= https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Script/alidrive.js
+
+http-response ^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Script/alidrive.js, requires-body=true, timeout=10, tag=alidrive
+
+[MITM]
+
+hostname = api.aliyundrive.com
