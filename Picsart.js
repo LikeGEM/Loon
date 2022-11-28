1990bb4c6a6482d946609d8a34c9b1c970454029
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-29 00:23:16 +0800

    Create Picsart.js

---
+++ Picsart.js
@@ -0,0 +1,14 @@
+#!name= Picsart美易
+#!desc= Picsart美易解锁订阅
+#!homepage= https://ocd0522.tk/ddgksf2013
+#!author= ddgksf2013
+#!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/Picsart.png
+
+[Script]
+
+# ～ Picsart美易解鎖會員權限（2022-02-19）@ddgksf2013
+http-response ^https?:\/\/api\.meiease\.cn\/shop\/subscription\/validate script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/picsart.js, requires-body=true, timeout=10, tag=picsart
+
+[MITM]
+
+hostname = api.meiease.cn
