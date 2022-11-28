a620ce3e618941e6a394dc3611cf910a9c3aeb53
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-28 23:59:12 +0800

    Create NotBoring.js

---
+++ NotBoring.js
@@ -0,0 +1,18 @@
+#!name= Not Boring
+#!desc= Not Boring四件套解锁订阅
+#!homepage= https://ocd0522.tk/ddgksf2013
+#!author= ddgksf2013
+#!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/NotBoring.png
+
+[Rewrite]
+
+# ～ Not Boring 四件套[天气、习惯、日历、时间] 解锁会员权限（2022-11-23）@ddgksf2013
+^https?:\/\/api-weather\.andy\.works\/v\d\/\w{13,18}$ reject-dict
+
+[Script]
+
+http-request ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/*) script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/notboring.js, requires-body=false, timeout=10, tag=notboring
+
+[MITM]
+
+hostname = api-weather.andy.works,api.revenuecat.com
