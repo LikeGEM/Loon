d50f4499c0d5810af36892bc79da33c542e14583
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-28 13:54:28 +0800

    Create 12306.js

---
+++ 12306.js
@@ -0,0 +1,13 @@
+#!name= 12306
+#!desc= 12306优化
+#!homepage= https://ocd0522.tk/ddgksf2013
+#!author= ddgksf2013
+#!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/12306.png
+
+[Script]
+
+http-response ^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Script/12306.js, requires-body=true, timeout=10, tag=12306
+
+[MITM]
+
+hostname = ad.12306.cn
