39d62bbeafc17e45004b656fa54d9d88df7b27fd
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-29 00:16:07 +0800

    Create aoruankoutu.js

---
+++ aoruankoutu.js
@@ -0,0 +1,14 @@
+#!name= 傲软抠图
+#!desc= 傲软抠图解锁订阅
+#!homepage= https://ocd0522.tk/ddgksf2013
+#!author= ddgksf2013
+#!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/aoruankoutu.png
+
+[Script]
+
+# ～ 傲软抠图解锁会员权限（2022-09-13）@ddgksf2013
+http-response https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations$ script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/apowersoft.js, requires-body=true, timeout=10, tag=apowersoft
+
+[MITM]
+
+hostname = *.aoscdn.com
