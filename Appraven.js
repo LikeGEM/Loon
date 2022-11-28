59b17b33bc734c1e27dd08470e7195e0bc063601
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-28 14:12:14 +0800

    Create Appraven.js

---
+++ Appraven.js
@@ -0,0 +1,14 @@
+#!name= Appraven
+#!desc= Appraven Pro
+#!homepage= https://ocd0522.tk/ddgksf2013
+#!author= ddgksf2013
+#!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/Appraven.png
+
+[Script]
+
+# ～ appraven（2022-03-22）@ddgksf2013
+http-response ^https?:\/\/appraven\.net\/AppRaven\/(app|social|buy) script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/appraven.js, requires-body=true, timeout=10, tag=appraven
+
+[MITM]
+
+hostname = appraven.net
