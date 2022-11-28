e8d9b6b7270b3077a0fe3445514ec3e282837246
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-29 00:04:55 +0800

    Create TopWidgets.js

---
+++ TopWidgets.js
@@ -0,0 +1,14 @@
+#!name= TopWidgets
+#!desc= TopWidgets解锁订阅
+#!homepage= https://ocd0522.tk/ddgksf2013
+#!author= ddgksf2013
+#!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/TopWidgets.png
+
+[Script]
+
+# ～ TopWidgets☆永久VIP（2022-10-30）@ddgksf2013
+http-response ^https?:\/\/.*xiaozujian\.com\/api\/app\/config\/userConfig script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/topwidgets.js, requires-body=true, timeout=10, tag=topwidgets
+
+[MITM]
+
+hostname = *xiaozujian.com
