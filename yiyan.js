7425e9ee596899c9eda4d602a49a0c1fd1e21c22
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-30 16:30:26 +0800

    Update yiyan.js

--- yiyan.js
+++ yiyan.js
@@ -1,13 +1,12 @@
 #!name= 一言
 #!desc= 一言解锁订阅
-#!homepage= https://ocd0522.tk/ddgksf2013
-#!author= ddgksf2013
+#!homepage= https://github.com/LikeGEM
+#!author=LikeGEM
 #!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/yiyan.png
 
 [Script]
 
-# ～ 一言解锁会员权限（2022-07-21）@ddgksf2013
-http-response ^https:\/\/app\.yiyan\.art\/yiyan\/ script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/yiyan.js, requires-body=true, timeout=10, tag=yiyan
+http-response ^https:\/\/app\.yiyan\.art\/yiyan\/ script-path=https://raw.githubusercontent.com/LikeGEM/QuanCX/main/yiyan.js, requires-body=true, timeout=10, tag=yiyan
 
 [MITM]
 
