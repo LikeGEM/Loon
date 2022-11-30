27ab4682c682a28eaa22b41b3c6b8a10fcd5b767
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-30 23:03:52 +0800

    Update caiyuntianqi.js

--- caiyuntianqi.js
+++ caiyuntianqi.js
@@ -1,13 +1,12 @@
 #!name= 彩云天气
 #!desc= 彩云天气解锁订阅
-#!homepage= https://ocd0522.tk/ddgksf2013
-#!author= ddgksf2013
+#!homepage= https://github.com/LikeGEM
+#!author= LikeGEM
 #!icon= https://raw.githubusercontent.com/LikeGEM/Loon/main/png/caiyuntianqi.png
 
 [Script]
 
-# >  彩云天气 （2020-11-21） by ddgksf2013
-http-response https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user) script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/caiyun.js, requires-body=true, timeout=10, tag=caiyun
+http-response ^https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user) script-path=https://raw.githubusercontent.com/LikeGEM/QuanCX/main/caiyuntianqi.js, requires-body=true, timeout=10, tag=caiyuntianqi
 
 [MITM]
 
